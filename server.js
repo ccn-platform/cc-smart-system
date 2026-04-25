e12dabaa640a8d927cbfeeaaacd1383fd2b0f315
ccn-platform <ibracredity@gmail.com>
2026-04-25 05:48:43 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,19 @@
 const app = require("./app");
 const connectDB = require("./config/db");
 
-// connect database
-connectDB();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Startup failed:", error.message);
+  }
+};
+
+startServer();
