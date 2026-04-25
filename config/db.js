421b68daa81f2f8374481cd96c5c04364c25934c
ccn-platform <ibracredity@gmail.com>
2026-04-25 01:33:20 -0700

    Change MongoDB connection URI to use MONGO_URI_ATLAS

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI_ATLAS);
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error.message);
