f7c6b5468a61c78ac33c478426ffcc34bb8fa6a0
ccn-platform <ibracredity@gmail.com>
2026-04-25 05:54:29 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_ATLAS);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error.message);
+    process.exit(1);
   }
 };
 
