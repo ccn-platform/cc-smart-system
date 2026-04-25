67748c1e2c472bc3c7ce6b03de606f199a4098fe
ccn-platform <ibracredity@gmail.com>
2026-04-25 06:05:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 
 const app = require("./app");
 const connectDB = require("./config/db");
-
+const { markOverdueLoans } = require("./services/overdueService");
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
     await connectDB();
-
+setInterval(() => {
+  markOverdueLoans();
+}, 60000);
     app.listen(PORT, () => {
       console.log(`Server running on ${PORT}`);
     });
