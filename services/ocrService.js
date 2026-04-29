76d2c4cd30c56f32b895fb3495f6f35b4acf7893
ccn-platform <ibracredity@gmail.com>
2026-04-28 20:11:43 -0700

    Update API endpoint for Gemini model in OCR service

--- services/ocrService.js
+++ services/ocrService.js
@@ -10,7 +10,7 @@ async (file) => {
       );
 
  const url =
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-8b:generateContent?key=${process.env.GEMINI_API_KEY}`;
 
     const prompt = `
 Read this supplier order image carefully.
