69d06166ad02e0e76b8b03374f061ef3cb2cedcb
ccn-platform <ibracredity@gmail.com>
2026-05-01 20:02:19 -0700

    Add subscription plans with pricing and duration

---
+++ utils/subscriptionPlans.js
@@ -0,0 +1,20 @@
+const PLANS = {
+  weekly: {
+    price: 2000,
+    days: 7
+  },
+  monthly: {
+    price: 5200,
+    days: 30
+  },
+  six_months: {
+    price: 30000,
+    days: 180
+  },
+  yearly: {
+    price: 60000,
+    days: 365
+  }
+};
+
+module.exports = PLANS;
