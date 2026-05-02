c3f9b3436a62e2031d7442270ed26062e4c4f826
ccn-platform <ibracredity@gmail.com>
2026-05-01 20:53:59 -0700

    Implement ClickPesaAuthService for token generation

---
+++ services/clickpesaAuthService.js
@@ -0,0 +1,30 @@
+ 
+ const axios = require("axios");
+
+class ClickPesaAuthService {
+
+ async getToken() {
+
+  const response = await axios.post(
+   `${process.env.CLICKPESA_BASE_URL}/third-parties/generate-token`,
+   {},
+   {
+    headers: {
+     "api-key": process.env.CLICKPESA_API_KEY,
+     "client-id": process.env.CLICKPESA_CLIENT_ID
+    }
+   }
+  );
+
+  console.log("CLICKPESA TOKEN RESPONSE:", response.data);
+
+  return response.data.token;
+ }
+
+}
+
+module.exports = new ClickPesaAuthService();
+
+
+
+ 
