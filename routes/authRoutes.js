7b7872fad6273596e468b9db4fdab5254801f551
ccn-platform <ibracredity@gmail.com>
2026-05-04 03:19:55 -0700

    Remove owner restriction from add-staff route
    Removed owner protection from the add-staff route.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -18,6 +18,6 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 
 // 🔥 OWNER ONLY
-router.post("/add-staff", protect, onlyOwner, addStaff);
+ router.post("/add-staff", addStaff);
 
 module.exports = router;
