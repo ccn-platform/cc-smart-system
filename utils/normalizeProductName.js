3367b31732be7917572eec00880d29f4433a70e3
ccn-platform <ibracredity@gmail.com>
2026-05-05 12:35:10 -0700

    Update normalizeProductName.js

--- utils/normalizeProductName.js
+++ utils/normalizeProductName.js
@@ -1,50 +1,118 @@
- const normalizeProductName = (name) => {
-  let text = String(name)
+  const normalizeProductName =
+(name) => {
+  let text =
+    String(name)
     .toLowerCase()
 
     // symbols
-    .replace(/[^a-z0-9 ]/g, " ")
+    .replace(
+      /[^a-z0-9 ]/g,
+      " "
+    )
 
-    // bidhaa fixes
-    .replace(/\bsukr\b/g, "sukari")
-    .replace(/\bsukal\b/g, "sukari")
-    .replace(/\bsugar\b/g, "sukari")
+    // common product shortcuts
+    .replace(
+      /\bsukr\b/g,
+      "sukari"
+    )
+    .replace(
+      /\bsukal\b/g,
+      "sukari"
+    )
+    .replace(
+      /\bsugar\b/g,
+      "sukari"
+    )
 
-    .replace(/\bmcel\b/g, "mchele")
-    .replace(/\bmchle\b/g, "mchele")
+    .replace(
+      /\bmcel\b/g,
+      "mchele"
+    )
+    .replace(
+      /\bmchle\b/g,
+      "mchele"
+    )
 
-    .replace(/\bmaha\b/g, "maharage")
-    .replace(/\bharage\b/g, "maharage")
+    .replace(
+      /\bmaha\b/g,
+      "maharage"
+    )
+    .replace(
+      /\bharage\b/g,
+      "maharage"
+    )
 
-    .replace(/\bsbn\b/g, "sabuni")
-    .replace(/\bsabn\b/g, "sabuni")
+    .replace(
+      /\bsbn\b/g,
+      "sabuni"
+    )
+    .replace(
+      /\bsabn\b/g,
+      "sabuni"
+    )
 
-    .replace(/\bungaa\b/g, "unga")
+    .replace(
+      /\bungaa\b/g,
+      "unga"
+    )
 
-    // 🔥 mpya (important sana)
-    .replace(/\bndg\b/g, "ndogo")
-    .replace(/\bextra\b/g, "extra")
+    // remove units
+    .replace(
+      /\bkg\b/g,
+      ""
+    )
+    .replace(
+      /\bkilo\b/g,
+      ""
+    )
+    .replace(
+      /\bgm\b/g,
+      ""
+    )
+    .replace(
+      /\bltr\b/g,
+      ""
+    )
+    .replace(
+      /\bml\b/g,
+      ""
+    )
+    .replace(
+      /\bpkt\b/g,
+      ""
+    )
+    .replace(
+      /\bpack\b/g,
+      ""
+    )
+    .replace(
+      /\bpc\b/g,
+      ""
+    )
+    .replace(
+      /\bpcs\b/g,
+      ""
+    )
+    .replace(
+      /\bbox\b/g,
+      ""
+    )
 
-    // remove units only
-    .replace(/\bkg\b/g, "")
-    .replace(/\bkilo\b/g, "")
-    .replace(/\bgm\b/g, "")
-    .replace(/\bltr\b/g, "")
-    .replace(/\bml\b/g, "")
-    .replace(/\bpkt\b/g, "")
-    .replace(/\bpack\b/g, "")
-    .replace(/\bpc\b/g, "")
-    .replace(/\bpcs\b/g, "")
-    .replace(/\bbox\b/g, "")
-
-    // ❌ USIONDOE numbers
-    // .replace(/\b\d+\b/g, "")
+    // remove numbers
+    .replace(
+      /\b\d+\b/g,
+      ""
+    )
 
     // spaces
-    .replace(/ +/g, " ")
+    .replace(
+      / +/g,
+      " "
+    )
     .trim();
 
   return text;
 };
 
-module.exports = normalizeProductName;
+module.exports =
+normalizeProductName;
