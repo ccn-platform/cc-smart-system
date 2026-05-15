da2487512430947ce5f91d39120825057fa5a331
ccn-platform <ibracredity@gmail.com>
2026-05-15 09:04:05 +0300

    Update overdueService.js

--- services/overdueService.js
+++ services/overdueService.js
@@ -1,53 +1,59 @@
-const DebtLoan =
-require("../models/DebtLoan");
+ const DebtLoan =
+  require("../models/DebtLoan");
 
 const CustomerIdentity =
-require("../models/CustomerIdentity");
+  require("../models/CustomerIdentity");
 
 const markOverdueLoans =
-async () => {
-  try {
-    const today =
-      new Date();
+  async () => {
+    try {
+      const today =
+        new Date();
 
-    const loans =
-      await DebtLoan.find({
-        status:
-          "active",
-        dueDate: {
-          $lt: today
-        },
-        balanceAmount: {
-          $gt: 0
-        }
-      });
-
-    for (const loan of loans) {
-      loan.status =
-        "overdue";
-
-      await loan.save();
-
-      await CustomerIdentity.findByIdAndUpdate(
-        loan.customer,
-        {
-          $inc: {
-            overdueLoans: 1
+      const loans =
+        await DebtLoan.find({
+          status: "active",
+          dueDate: {
+            $lt: today
+          },
+          balanceAmount: {
+            $gt: 0
           }
-        }
+        }).select("_id customer");
+
+      for (const loan of loans) {
+        await DebtLoan.updateOne(
+          {
+            _id: loan._id
+          },
+          {
+            $set: {
+              status: "overdue"
+            }
+          }
+        );
+
+        await CustomerIdentity.findByIdAndUpdate(
+          loan.customer,
+          {
+            $inc: {
+              overdueLoans: 1
+            }
+          }
+        );
+      }
+
+      console.log(
+        "Overdue updated:",
+        loans.length
+      );
+    } catch (error) {
+      console.log(
+        "OVERDUE ERROR:",
+        error.message
       );
     }
-
-    console.log(
-      "Overdue updated:",
-      loans.length
-    );
-  } catch (error) {
-    console.log(
-      error.message
-    );
-  }
-};
+  };
 
 module.exports = {
   markOverdueLoans
