7cd372f7f1c877d1daf6ed133a2374db37da424f
ccn-platform <ibracredity@gmail.com>
2026-05-20 07:18:12 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,49 +1,56 @@
  const express = require("express");
 const router = express.Router();
 
-const authController =
-  require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  addStaff,
+  getStaff,
+  deleteAccount,
+  updateProfile,
+  deleteStaff
+} = require("../controllers/authController");
 
-const middleware =
-  require("../middleware/authMiddleware");
+const {
+  protect,
+  onlyOwner
+} = require("../middleware/authMiddleware");
 
-console.log({
-  registerUser:
-    typeof authController.registerUser,
-  loginUser:
-    typeof authController.loginUser,
-  addStaff:
-    typeof authController.addStaff,
-  getStaff:
-    typeof authController.getStaff,
-  protect:
-    typeof middleware.protect,
-  onlyOwner:
-    typeof middleware.onlyOwner
-});
-
-router.post(
-  "/register",
-  authController.registerUser
-);
-
-router.post(
-  "/login",
-  authController.loginUser
-);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 router.post(
   "/add-staff",
-  middleware.protect,
-  middleware.onlyOwner,
-  authController.addStaff
+  protect,
+  onlyOwner,
+  addStaff
 );
 
 router.get(
   "/staff",
-  middleware.protect,
-  middleware.onlyOwner,
-  authController.getStaff
+  protect,
+  onlyOwner,
+  getStaff
+);
+
+router.delete(
+  "/staff/:staffId",
+  protect,
+  onlyOwner,
+  deleteStaff
+);
+
+router.put(
+  "/profile",
+  protect,
+  updateProfile
+);
+
+router.delete(
+  "/account",
+  protect,
+  onlyOwner,
+  deleteAccount
 );
 
 module.exports = router;
